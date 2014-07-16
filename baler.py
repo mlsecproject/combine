791856c6ac039dacd4f5160d4078adb0b6d9bab1
Kyle Maxwell <kylem@xwell.org>
2014-07-15 23:02:15 -0500

    Make it so

---
+++ baler.py
@@ -0,0 +1,23 @@
+import csv
+import json
+
+
+def bale_csv(harvest, output_file):
+    with open(output_file, 'wb') as csv_file:
+        bale_writer = csv.writer(csv_file)
+
+        # header row
+        bale_writer.writerow(('entity', 'type', 'direction', 'source', 'notes', 'date'))
+        bale_writer.writerows(harvest)
+
+
+def bale(input_file, output_file, output_format):
+    with open(input_file, 'rb') as f:
+        harvest = json.load(f)
+
+    format_funcs = { 'csv': bale_csv }
+    format_funcs[output_format](harvest, output_file)
+
+
+if __name__ == "__main__":
+    bale('crop.json', 'harvest.csv', 'csv')
