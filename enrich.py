fd63064516a4c1d484517602179aec90248f4498
Kyle Maxwell <kylem@xwell.org>
2014-07-31 02:00:05 -0500

    Clean up and add stubs to support #22

--- enrich.py
+++ enrich.py
@@ -1,16 +1,31 @@
 #! /usr/bin/env python
 
 
+def maxmind(address):
+    pass
+
+
+def maxhits(dns_records):
+    pass
+
+
+def dnsdb(address, record_type):
+    pass
+
+
 def enrich_IPv4(address):
     (ASN, AS_name, country) = maxmind(address)
     hostname = maxhits(dnsdb(each, "PTR"))
     return (address, ASN, AS_name, country, hostname)
 
 
+# honestly this bit can wait since we're not processing DNS right now
 def enrich_DNS(address):
     for ip_addr in dnsdb(address, "A")
+        # how do we add this back?
         harvest += IPv4(ip_addr, host=FQDN.value)
-        enrich(everyone)
+        # and return this?
+        enrich_IPv4(ip_addr)
 
         
 def enrich(data):
@@ -23,5 +38,3 @@ def enrich(data):
         else:
             print "Can't determine type: %s" % indicator
     return enrichment
-
-
