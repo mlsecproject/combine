bda894d350a68bbdb0531c8476175a45a0a415ce
Kyle Maxwell <kylem@xwell.org>
2014-07-31 02:10:09 -0500

    Renamed and reserved ranges noted

--- winnow.py
+++ winnow.py
@@ -38,3 +38,34 @@ def enrich(data):
         else:
             print "Can't determine type: %s" % indicator
     return enrichment
+
+
+def reserved(address):
+    # from http://en.wikipedia.org/wiki/Reserved_IP_addresses:
+    ranges = ['0.0.0.0/8', 
+              '10.0.0.0/8',
+              '100.64.0.0/10',
+              '127.0.0.0/8',
+              '169.254.0.0/16',
+              '172.16.0.0/12',
+              '192.0.0.0/29',
+              '192.0.2.0/24',
+              '192.88.99.0/24',
+              '192.168.0.0/16',
+              '198.18.0.0/15',
+              '198.51.100.0/24',
+              '203.0.113.0/24',
+              '224.0.0.0/4',
+              '233.252.0.0/24',
+              '240.0.0.0/4',
+              '255.255.255.254/32']
+
+
+def filter(data):
+    harvest = []
+    for address in data:
+        if indicator_type(address) == IPv4:
+            # this step is because of shitty SANS data
+            ip_addr = ip_normalize(address)
+            if not reserved(ip_addr):
+                harvest.append(ip_addr)
