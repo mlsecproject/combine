61e5be6cfd37b884de7e9cf3ca3abff7607d4613
Kyle Maxwell <kmaxwell@verisign.com>
2014-08-02 21:22:51 -0500

    Fixes #26

--- thresher.py
+++ thresher.py
@@ -69,6 +69,10 @@ def process_alienvault(response, source, direction):
         if not line.startswith('#') and len(line) > 0:
             i = line.partition('#')[0].strip()
             note = line.split('#')[3].strip()
+            if 'Scanning Host' in note or 'Spamming' in note:
+                direction = 'inbound'
+            elif 'Malware' in note or 'C&C' in note or 'APT' in note:
+                direction = 'outbound'
             data.append((i, indicator_type(i), direction, source, note, '%s' % datetime.date.today()))
     return data
 
