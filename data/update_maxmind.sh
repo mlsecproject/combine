6c176da74a4d89463fab8da01418b909f4e42e4a
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2014-12-17 16:38:57 -0600

    changed the script to add GeoIPASNum2.zip and to further facilitate running this in crontab

--- data/update_maxmind.sh
+++ data/update_maxmind.sh
@@ -4,6 +4,11 @@
 # you can set this up in the crontab to update it daily:
 # 0 0 * * * [combine_folder]/data/update_maxmind.sh
 
+# caveat: if you do set this to update in an automated fashion, please uncomment the following line:
+# cd [combine_folder]/data/
+
 wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz && gunzip -f GeoIP.dat.gz
 
 wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz && gunzip -f GeoLiteCity.dat.gz
+
+wget -q http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum2.zip && unzip -qqo GeoIPASNum2.zip && rm GeoIPASNum2.zip
