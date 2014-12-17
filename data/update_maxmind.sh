4fee37ec59625485c29a9e0f3dc77c29f333abf6
Paul Poputa-Clean <Paul.Poputa-Clean@dvn.com>
2014-12-16 23:12:34 -0600

    Small baler error codes updates, suppressed output for the maxmind updater

--- data/update_maxmind.sh
+++ data/update_maxmind.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com"
+# This product includes GeoLite data created by MaxMind, available from http://www.maxmind.com
 
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz && gunzip -f GeoIP.dat.gz
+# you can set this up in the crontab to update it daily:
+# 0 0 * * * [combine_folder]/data/update_maxmind.sh
 
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz && gunzip -f GeoLiteCity.dat.gz
+wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz && gunzip -f GeoIP.dat.gz
+
+wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz && gunzip -f GeoLiteCity.dat.gz
