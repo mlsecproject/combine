b3cb81b1c446a2f7bf08f2d63f2dd3f37c98f59a
Kyle Maxwell <kmaxwell@verisign.com>
2014-12-26 14:31:39 -0600

    PEP8 cleanup

--- baler.py
+++ baler.py
@@ -13,8 +13,8 @@ import threading
 from logger import get_logger
 import logging
 
-
 logger = get_logger('baler')
+
 
 def tiq_output(reg_file, enr_file):
     config = ConfigParser.SafeConfigParser()
@@ -43,8 +43,8 @@ def tiq_output(reg_file, enr_file):
     outbound_data = [row for row in reg_data if row[2] == 'outbound']
 
     try:
-        bale_reg_csvgz(inbound_data, os.path.join(tiq_dir, 'raw', 'public_inbound', today+'.csv.gz'))
-        bale_reg_csvgz(outbound_data, os.path.join(tiq_dir, 'raw', 'public_outbound', today+'.csv.gz'))
+        bale_reg_csvgz(inbound_data, os.path.join(tiq_dir, 'raw', 'public_inbound', today + '.csv.gz'))
+        bale_reg_csvgz(outbound_data, os.path.join(tiq_dir, 'raw', 'public_outbound', today + '.csv.gz'))
     except:
         pass
 
@@ -52,8 +52,8 @@ def tiq_output(reg_file, enr_file):
     outbound_data = [row for row in enr_data if row[2] == 'outbound']
 
     try:
-        bale_enr_csvgz(inbound_data, os.path.join(tiq_dir, 'enriched', 'public_inbound', today+'.csv.gz'))
-        bale_enr_csvgz(outbound_data, os.path.join(tiq_dir, 'enriched', 'public_outbound', today+'.csv.gz'))
+        bale_enr_csvgz(inbound_data, os.path.join(tiq_dir, 'enriched', 'public_inbound', today + '.csv.gz'))
+        bale_enr_csvgz(outbound_data, os.path.join(tiq_dir, 'enriched', 'public_outbound', today + '.csv.gz'))
     except:
         pass
 
@@ -92,6 +92,7 @@ def bale_enr_csv(harvest, output_file):
         bale_writer.writerow(('entity', 'type', 'direction', 'source', 'notes', 'date', 'asnumber', 'asname', 'country', 'host', 'rhost'))
         bale_writer.writerows(harvest)
 
+
 def bale_enr_csvgz(harvest, output_file):
     """ output the data as an enriched gziped csv file"""
     logger.info('Output enriched data as GZip CSV to %s' % output_file)
@@ -102,96 +103,100 @@ def bale_enr_csvgz(harvest, output_file):
         bale_writer.writerow(('entity', 'type', 'direction', 'source', 'notes', 'date', 'asnumber', 'asname', 'country', 'host', 'rhost'))
         bale_writer.writerows(harvest)
 
-def bale_CRITs_indicator(base_url,data,indicator_que):
+
+def bale_CRITs_indicator(base_url, data, indicator_que):
     """ One thread of adding indicators to CRITs"""
     while not indicator_que.empty():
-        indicator=indicator_que.get()
+        indicator = indicator_que.get()
         if indicator[1] == 'IPv4':
             # using the IP API
-            url=base_url+'ips/'
-            data['add_indicator']="true"
-            data['ip']=indicator[0]
-            data['ip_type']='Address - ipv4-addr'
-            data['reference']=indicator[3]
+            url = base_url + 'ips/'
+            data['add_indicator'] = "true"
+            data['ip'] = indicator[0]
+            data['ip_type'] = 'Address - ipv4-addr'
+            data['reference'] = indicator[3]
             # getting the source automatically:
-            source=re.findall(r'\/\/(.*?)\/',data['reference'])
+            source = re.findall(r'\/\/(.*?)\/', data['reference'])
             if source:
-                data['source']=source[0]
-            res = requests.post(url,data=data,verify=False)
-            if not res.status_code in [201,200,400]:
+                data['source'] = source[0]
+            res = requests.post(url, data=data, verify=False)
+            if not res.status_code in [201, 200, 400]:
                 logger.info("Issues with adding: %s" % data['ip'])
         elif indicator[1] == "FQDN":
             # using the Domain API
-            url=base_url+'domains/'
-            data['add_indicator']="true"
-            data['domain']=indicator[0]
-            data['reference']=indicator[3]
+            url = base_url + 'domains/'
+            data['add_indicator'] = "true"
+            data['domain'] = indicator[0]
+            data['reference'] = indicator[3]
             # getting the source automatically:
-            source=re.findall(r'\/\/(.*?)\/',data['reference'])
+            source = re.findall(r'\/\/(.*?)\/', data['reference'])
             if source:
-                data['source']=source[0]
-            res = requests.post(url,data=data,verify=False)
-            if not res.status_code in [201,200,400]:
+                data['source'] = source[0]
+            res = requests.post(url, data=data, verify=False)
+            if not res.status_code in [201, 200, 400]:
                 logger.info("Issues with adding: %s" % data['domain'])
         else:
-            logger.info("don't yet know what to do with: %s[%s]" % (indicator[1],indicator[0]))
+            logger.info("don't yet know what to do with: %s[%s]" % (indicator[1], indicator[0]))
 
-def bale_CRITs(harvest,filename):
+
+def bale_CRITs(harvest, filename):
     """ taking the output from combine and pushing it to the CRITs web API"""
     # checking the minimum requirements for parameters
     # it would be nice to have some metadata on the feeds that can be imported in the intel library:
     #   -> confidence
     #   -> type of feed (bot vs spam vs ddos, you get the picture)
-    data={'confidence':'medium'}
-    start_time=time.time()
+    data = {'confidence': 'medium'}
+    start_time = time.time()
     config = ConfigParser.SafeConfigParser()
     cfg_success = config.read('combine.cfg')
     if not cfg_success:
         logger.error('tiq_output: Could not read combine.cfg.\n')
         logger.error('HINT: edit combine-example.cfg and save as combine.cfg.\n')
         return
-    if config.has_option('Baler','crits_username'):
-        data['username']=config.get('Baler', 'crits_username')
+    if config.has_option('Baler', 'crits_username'):
+        data['username'] = config.get('Baler', 'crits_username')
     else:
         raise 'Please check the combine.cnf file for the crits_username field in the [Baler] section'
-    if config.has_option('Baler','crits_api_key'):
-        data['api_key']=config.get('Baler', 'crits_api_key')
+    if config.has_option('Baler', 'crits_api_key'):
+        data['api_key'] = config.get('Baler', 'crits_api_key')
     else:
         raise 'Please check the combine.cnf file for the crits_api_key field in the [Baler] section'
-    if config.has_option('Baler','crits_campaign'):
-        data['campaign']=config.get('Baler', 'crits_campaign')
+    if config.has_option('Baler', 'crits_campaign'):
+        data['campaign'] = config.get('Baler', 'crits_campaign')
     else:
         logger.info('Lacking a campaign name, we will default to "combine." Errors might ensue if it does not exist in CRITs')
-        data['campaign']='combine'
-    if config.has_option('Baler','crits_url'):
-        base_url=config.get('Baler','crits_url')
+        data['campaign'] = 'combine'
+    if config.has_option('Baler', 'crits_url'):
+        base_url = config.get('Baler', 'crits_url')
     else:
         raise 'Please check the combine.cnf file for the crits_url field in the [Baler] section'
-    if config.has_option('Baler','crits_maxThreads'):
-        maxThreads=int(config.get('Baler', 'crits_maxThreads'))
+    if config.has_option('Baler', 'crits_maxThreads'):
+        maxThreads = int(config.get('Baler', 'crits_maxThreads'))
     else:
         logger.info('No number of maximum Threads has been given, defaulting to 10')
-        maxThreads=10
+        maxThreads = 10
 
-    data['source']='Combine'
-    data['method']='trawl'
+    data['source'] = 'Combine'
+    data['method'] = 'trawl'
 
     # initializing the Queue to the list of indicators in the harvest
-    ioc_queue=Queue()
+    ioc_queue = Queue()
     for indicator in harvest:
         ioc_queue.put(indicator)
-    total_iocs=ioc_queue.qsize()
+    total_iocs = ioc_queue.qsize()
 
     for x in range(maxThreads):
-        th=threading.Thread(target=bale_CRITs_indicator, args=(base_url,data,ioc_queue))
+        th = threading.Thread(target=bale_CRITs_indicator, args=(base_url, data, ioc_queue))
         th.start()
 
     for x in threading.enumerate():
-        if x.name=="MainThread":
+        if x.name == "MainThread":
             continue
         x.join()
 
-    logger.info('Output %d indicators to CRITs using %d threads. Operation tool %d seconds\n' % (total_iocs,maxThreads,time.time()-start_time))
+    logger.info('Output %d indicators to CRITs using %d threads. Operation tool %d seconds\n' %
+                (total_iocs, maxThreads, time.time() - start_time))
+
 
 def bale(input_file, output_file, output_format, is_regular):
     config = ConfigParser.SafeConfigParser()
@@ -207,9 +212,9 @@ def bale(input_file, output_file, output_format, is_regular):
 
     # TODO: also need plugins here (cf. #23)
     if is_regular:
-        format_funcs = {'csv': bale_reg_csv,'crits':bale_CRITs}
+        format_funcs = {'csv': bale_reg_csv, 'crits': bale_CRITs}
     else:
-        format_funcs = {'csv': bale_enr_csv,'crits':bale_CRITs}
+        format_funcs = {'csv': bale_enr_csv, 'crits': bale_CRITs}
     format_funcs[output_format](harvest, output_file)
 
 if __name__ == "__main__":
