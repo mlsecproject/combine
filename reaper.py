a703bd9617b83dc53a94caade360bcb18d5e485f
Mike Sconzo <msconzo@bit9.com>
2015-02-12 13:24:48 -0600

    code cleanup and reduced logging noise

--- reaper.py
+++ reaper.py
@@ -6,14 +6,16 @@ import logging
 from yapsy.PluginManager import PluginManager
 from logbook.compat import redirect_logging
 from logging import getLogger
-
-#requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.SecurityWarning)
+import uniaccept
 
 redirect_logging()
 
+#logging.getLogger('yapsy').setLevel(logging.DEBUG)
+logging.getLogger('yapsy').setLevel(logging.INFO)
+logging.getLogger('requests').setLevel(logging.CRITICAL)
 logger = getLogger('reaper')
-logging.getLogger('yapsy').setLevel(logging.DEBUG)
 
+# This only works with versions in github, not available via released versions in pip
 def exception_handler(request, exception):
     logger.error("Request %r failed: %r" % (request, exception))
 
@@ -28,19 +30,8 @@ def reap(file_name):
     inbound_url_file = config.get('Reaper', 'inbound_urls')
     outbound_url_file = config.get('Reaper', 'outbound_urls')
     plugin_dir = config.get('Reaper', 'plugin_directory')
-
-    try:
-        with open(inbound_url_file, 'rb') as f:
-    	    inbound_urls = [url.rstrip('\n') for url in f.readlines()]
-    except EnvironmentError as e:
-        logger.error('Reaper: Error while opening "%s" - %s' % (inbound_url_file, e.strerror))
-        return
-
-    try:
-        with open(outbound_url_file, 'rb') as f:
-            outbound_urls = [url.rstrip('\n') for url in f.readlines()]
-    except EnvironmentError as e:
-        logger.error('Reaper: Error while opening "%s" - %s' % (outbound_url_file, e.strerror))
+    if plugin_dir == None or plugin_dir == '':
+        logger.error("Thresher: Couldn't find plugins for processing")
         return
 
     ## Setting the User-Agent to something spiffy
@@ -52,73 +43,27 @@ def reap(file_name):
     manager.setPluginPlaces([plugin_dir])
     manager.collectPlugins()
 
-    #inbound_files = []
-    #inbound_reqs = []
-    #outbound_files = []
-    #outbound_reqs = []
-    #hash_files = []
-    #hash_reqs = []
     reqs = []
     files = []
     
     # Loop through all the plugins and gather the URLs
     for plugin in manager.getAllPlugins():
-        direction = plugin.plugin_object.get_direction()
-        logger.info(direction)
+        logger.info('Processing: ' + plugin.plugin_object.get_name())
         for url in plugin.plugin_object.get_URLs():
             if url.startswith('file://'):
                 files.append(url.partition('://')[2])
             else:
-                reqs.append(grequests.get(url, headers=headers))
-            #if direction == 'inbound':
-            #    if url.startswith('file://'):
-            #        inbound_files.append(url.partition('://')[2])
-            #    else:
-            #        inbound_reqs.append(grequests.get(url, headers=headers))
-            #elif direction == 'outbound':
-            #    logger.info(url)
-            #    if url.startswith('file://'):
-            #        outbound_files.append(url.partition('://')[2])
-            #    else:
-            #        outbound_reqs.append(grequests.get(url, headers=headers, verify=False))
-            #elif direction == 'hash':
-            #    if url.startswith('file://'):
-            #        hash_files.append(url.partition('://')[2])
-            #    else:
-            #        hash_reqs.append(grequests.get(url, headers=headers))
+                try:
+                    reqs.append(grequests.get(url, headers=headers))
+                except Exception as e:
+                    pass
 
-    #inbound_responses = grequests.map(inbound_reqs, exception_handler=exception_handler)
-    #outbound_responses = grequests.map(outbound_reqs, exception_handler=exception_handler)
-    #hash_responses = grequests.map(hash_reqs, exception_handler=exception_handler)
-    responses = grequests.map(reqs, exception_handler=exception_handler)
+    responses = grequests.map(reqs)
+    # Exception handler requires a version > 0.2.0
+    #responses = grequests.map(reqs, exception_handler=exception_handler)
 
-    #inbound_harvest = [(response.url, response.status_code, response.text) for response in inbound_responses if response]
-    #outbound_harvest = [(response.url, response.status_code, response.text) for response in outbound_responses if response]
-    #hash_harvest = [(response.url, response.status_code, response.text) for response in hash_responses if response]
     harvest = [(response.url, response.status_code, response.text) for response in responses if response]
 
-    #for each in inbound_files:
-    #    try:
-    #        with open(each,'rb') as f:
-    #            inbound_harvest.append(('file://'+each, 200, f.read()))
-    #    except IOError as e:
-    #        assert isinstance(logger, logging.Logger)
-    #        logger.error('Reaper: Error while opening "%s" - %s' % (each, e.strerror))
-    #for each in outbound_files:
-    #    try:
-    #        with open(each,'rb') as f:
-    #            outbound_harvest.append(('file://'+each, 200, f.read()))
-    #    except IOError as e:
-    #        assert isinstance(logger, logging.Logger)
-    #        logger.error('Reaper: Error while opening "%s" - %s' % (each, e.strerror))
-    #for each in hash_files:
-    #    try:
-    #        with open(each,'rb') as f:
-    #            hash_harvest.append(('file://'+each, 200, f.read()))
-    #    except IOError as e:
-    #        assert isinstance(logger, logging.Logger)
-    #        logger.error('Reaper: Error while opening "%s" - %s' % (each, e.strerror))
-            
     for each in files:
         try:
             with open(each,'rb') as f:
