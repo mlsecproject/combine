73d6e112ddbdd50e5f203771ad3d07f3580ae583
Kyle Maxwell <kylem@xwell.org>
2015-03-17 22:20:05 -0500

    Refactor to support testing

--- combine.py
+++ combine.py
@@ -14,41 +14,59 @@ from winnower import winnow
 
 logger = get_logger()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--type', help="Specify output type. Currently supported: CSV and exporting to CRITs")
-parser.add_argument('-f', '--file', help="Specify output file. Defaults to harvest.FILETYPE")
-parser.add_argument('-d', '--delete', help="Delete intermediate files", action="store_true")
-parser.add_argument('-e', '--enrich', help="Enrich data", action="store_true")
-parser.add_argument('--tiq-test', help="Output in tiq-test format", action="store_true")
-args = parser.parse_args()
 
-possible_types = ['csv', 'json','crits']
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--type', help="Specify output type. Currently supported: CSV and exporting to CRITs")
+    parser.add_argument('-f', '--file', help="Specify output file. Defaults to harvest.FILETYPE")
+    parser.add_argument('-d', '--delete', help="Delete intermediate files", action="store_true")
+    parser.add_argument('-e', '--enrich', help="Enrich data", action="store_true")
+    parser.add_argument('--tiq-test', help="Output in tiq-test format", action="store_true")
+    return parser.parse_args()
 
-if not args.type:
-    out_type = 'csv'
-elif args.type.lower() not in possible_types:
-    sys.exit('Invalid file type specified. Possible types are: %s' % possible_types)
-else:
-    out_type = args.type.lower()
 
-if args.file:
-    out_file = args.file
-else:
-    out_file = 'harvest.'+out_type
+def get_type(args):
+    possible_types = ['csv', 'json', 'crits']
 
-reap('harvest.json')
-thresh('harvest.json', 'crop.json')
-bale('crop.json', out_file, out_type, True)
+    if not args.type:
+        out_type = 'csv'
+    elif args.type.lower() not in possible_types:
+        sys.exit('Invalid file type specified. Possible types are: {}'.format(possible_types))
+    else:
+        out_type = args.type.lower()
 
-if args.enrich or args.tiq_test:
-    winnow('crop.json', 'crop.json', 'enrich.json')
-    bale('enrich.json', 'enriched.'+out_type, out_type, False)
+    return out_type
 
-if args.tiq_test:
-    tiq_output('crop.json', 'enrich.json')
 
-if args.delete:
-    # be careful with this when we support a JSON output type
-    os.remove('harvest.json')
-    os.remove('crop.json')
-    os.remove('enrich.json')
+def get_file(args, out_type):
+    if args.file:
+        out_file = args.file
+    else:
+        out_file = 'harvest.' + out_type
+
+
+def main():
+    args = get_args()
+    out_type = get_type(args)
+    out_file = get_file(args)
+
+    reap('harvest.json')
+    thresh('harvest.json', 'crop.json')
+    bale('crop.json', out_file, out_type, True)
+
+    if args.enrich or args.tiq_test:
+        winnow('crop.json', 'crop.json', 'enrich.json')
+        bale('enrich.json', 'enriched.' + out_type, out_type, False)
+
+    if args.tiq_test:
+        tiq_output('crop.json', 'enrich.json')
+
+    if args.delete:
+        # be careful with this when we support a JSON output type
+        os.remove('harvest.json')
+        os.remove('crop.json')
+        os.remove('enrich.json')
+
+
+if __name__ == "__main__":
+    main()
